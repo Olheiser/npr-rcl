3003ce5e0374272f760ca4bf904e04e1eee40853
Olheiser <tanner.olheiser@gmail.com>
2025-01-16 18:44:48 -0600

    Meta data updates

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://torontocriminallaw.net'
+  const baseUrl = 'https://reginacriminallawyer.ca'
 
   return [
     // Homepage - Highest priority
