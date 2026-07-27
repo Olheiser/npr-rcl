7aef86b3e673a1dae3b091fbbdf81d0fd0c70da3
Olheiser <tanner.olheiser@gmail.com>
2026-07-27 12:45:04 -0600

    inserted GTM tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TJHK9V8');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        
         {/* Google Tag (gtag.js) */}
         <Script
           async src="https://www.googletagmanager.com/gtag/js?id=G-QWG610QH0W"
@@ -43,6 +60,16 @@ export default function RootLayout({
   />
       </head>
       <body id="root">
+      {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJHK9V8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
          {/* <PageHeader title="About" /> */}
         {children}
